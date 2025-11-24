50f2d89ab17e33f2b68345559f823e80b92acccc
oscarprdev <vlcoscar8@gmail.com>
2025-11-24 08:15:16 +0100

    Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  vite: {    plugins: [      tailwindcss(),    ],  },
+  css: ['./app/assets/css/main.css'],
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
